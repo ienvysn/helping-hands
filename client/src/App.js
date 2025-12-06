7d443eeb7c934c12f6758d3f6cb730ddd84f0b59
oatkrunch <suyalsahukhala787@gmail.com>
2025-12-06 20:34:39 +0545

    Landing Page finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
